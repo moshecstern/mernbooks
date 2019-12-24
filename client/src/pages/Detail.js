114e985e7ee155d198b528e31db1a8ff37031550
Moshe Stern <moshecstern@gmail.com>
2019-12-24 12:56:05 -0500

    2 pages working, not displaying series or volumes

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,21 +3,31 @@ import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
+import { List, ListItem } from "../components/List";
 
 class Detail extends Component {
   state = {
-    Character: {}
+    character: {},
+    volumes: {},
+    series: {}
   };
   // Add code to get the Character with an _id equal to the id in the route param
   // e.g. http://localhost:3000/Characters/:id
   // The Character id for this route can be accessed using this.props.match.params.id
   componentDidMount() {
-    this.loadMyCharacter();
+    this.loadMySeries();
+    // this.loadMyCharacter();
   }
 
-  loadMyCharacter = () => {
-    API.getCharacter(this.props.match.params.id)
-      .then(res => this.setState({ Character: res.data })
+  // loadMyCharacter = () => {
+  //   API.getCharacter(this.props.match.params.id)
+  //     .then(res => this.setState({ character: res.data })
+  //     )
+  //     .catch(err => console.log(err));
+  // };
+  loadMySeries = () => {
+    API.findCharacterBySeries(this.props.match.params.charactername, this.props.match.params.seriesname)
+      .then(res => this.setState({ volumes: res.data })
       )
       .catch(err => console.log(err));
   };
@@ -30,23 +40,39 @@ class Detail extends Component {
         <Row>
           <Col size="md-12">
             <Jumbotron>
-              <h1>
-                {this.state.Character.name} by {this.state.Character.info}
-              </h1>
+            {this.state.volumes.length ? (
+              <List>
+                {this.state.volumes.map(vol => (
+                  <ListItem key={vol._id}>
+                    {/* <Link to={"/series/"+ vol.seriesname+"/" character}> */}
+                      <strong>
+                        {vol.name}
+                        {/* <br></br>
+                        {vol.series} */}
+                        {console.log(vol.name)}
+                      </strong>
+                    {/* </Link> */}
+                    {/* <DeleteBtn onClick={() => this.deletecharacter(character._id)} /> */}
+                    {/* {character.series.map(series => <Link to={`/api/series/${series}/${character.name}`}>{series}</Link>)} */}
+                    {/* {character.series.map(series => {series})} */}
+                    
+                  </ListItem>
+                ))}
+             
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
             </Jumbotron>
           </Col>
         </Row>
         <Row>
           <Col size="md-10 md-offset-1">
-            <article>
-              <h1>Synopsis</h1>
-              <p>{this.state.Character.info}</p>
-            </article>
           </Col>
         </Row>
         <Row>
           <Col size="md-2">
-            <Link to="/">← Back to Authors</Link>
+            <Link to="/">← Back to Characters</Link>
           </Col>
         </Row>
       </Container>
@@ -55,3 +81,29 @@ class Detail extends Component {
 }
 
 export default Detail;
+
+
+{/* <h1>
+HELLO!
+{console.log(this.state.volumes)}
+{console.log(this.state.series.length)}
+{console.log(this.state.volumes)}
+{this.state.volumes.map(volume => (
+  <p>{volume.seriesname}</p>
+  <p>hello 2</p>
+))}
+</h1>
+<List>
+{this.state.volumes.map(volume => (
+<ListItem onClick={this.loadMyCharacter}>
+
+  <article>
+    <h1>Synopsis</h1>
+    <p>{volume}</p>
+    <p>HIII!</p>
+
+  </article>
+</ListItem>
+))}
+</List> 
+{console.log(this.state.volumes)} */}
--- controllers/seriesController.js
+++ controllers/seriesController.js
@@ -2,10 +2,22 @@ const db = require("../models");
 
 // Defining methods for the SeriessController
 module.exports = {
+  findCharacterBySeries: function(req, res) {
+    console.log(res)
+    console.log(req.params)
+    console.log(req.params.seriesname)
+    db.Series
+    .find({
+      series: req.params.seriesname,
+      character: req.params.charactername
+    })
+    .then(dbResult => res.json(dbResult))
+    .catch(err => res.status(422).json(err));
+  },
   findAll: function(req, res) {
     db.Series
       .find(req.query)
-      .sort({ date: -1 })
+      .sort({ name: 1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
