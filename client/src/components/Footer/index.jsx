f567693a17d202f25fe6ed130e9c686daa55c48c
liamwynne <liamdwynne5753@gmail.com>
2020-01-06 13:48:19 -0500

    color, padding of nav and footer fixed

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,9 +1,20 @@
 import React from "react";
-import { Grid, Typography, Link } from "@material-ui/core";
+import { Grid, Typography, Link, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => {
+  return {
+    title: {
+      backgroundColor: "#F2A74B",
+      padding: theme.spacing(2, 1, 2, 1)
+    }
+  };
+});
+
 
 const Footer = () => {
+  const classes = useStyles();
   return (
-    <Grid container justify="space-between" alignItems="baseline">
+    <Grid container justify="space-between" alignItems="baseline" className={classes.title}> 
       <Grid item>
         <Link href="https://github.com/moshecstern/mernbooks">
           View Repository
